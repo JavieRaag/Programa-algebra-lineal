39b14a6f67110a2f0b003f1b1a47a72b3f41b7a6
JavieRaag <144748730+JavieRaag@users.noreply.github.com>
2024-02-29 00:24:38 -0600

    Update codigo.cpp

--- codigo.cpp
+++ codigo.cpp
@@ -4,16 +4,16 @@
 using namespace std;
 
 int main() {
-    // Obtener el número de variables (n)
+    
     int n;
     cout << "Ingrese el número de variables: ";
     cin >> n;
 
-    // Crear matrices para almacenar los coeficientes y resultados
+    
     vector<vector<double>> coeficientes(n, vector<double>(n, 0.0));
     vector<double> resultados(n, 0.0);
 
-    // Ingresar los coeficientes del sistema de ecuaciones
+    
     cout << "Ingrese los coeficientes del sistema de ecuaciones:" << endl;
     for (int i = 0; i < n; ++i) {
         for (int j = 0; j < n; ++j) {
@@ -24,20 +24,20 @@ int main() {
         cin >> resultados[i];
     }
 
-    // Resolver el sistema de ecuaciones usando eliminación gaussiana
+    
     for (int i = 0; i < n; ++i) {
-        // Hacer que el coeficiente en la diagonal sea 1
+        
         double pivot = coeficientes[i][i];
         for (int j = 0; j < n; ++j) {
             coeficientes[i][j] /= pivot;
         }
         resultados[i] /= pivot;
 
-        // Hacer que los demás elementos de la columna sean 0
+        
         for (int k = 0; k < n; ++k) {
             if (k != i) {
                 double factor = coeficientes[k][i];
-                for (int j = 0; j < n; ++j) {
+                for (int j = 0; j < n; j++) {
                     coeficientes[k][j] -= factor * coeficientes[i][j];
                 }
                 resultados[k] -= factor * resultados[i];
@@ -45,7 +45,7 @@ int main() {
         }
     }
 
-    // Mostrar la solución
+   
     cout << "\nLa solución es:" << endl;
     for (int i = 0; i < n; ++i) {
         cout << "Variable " << i + 1 << ": " << resultados[i] << endl;
